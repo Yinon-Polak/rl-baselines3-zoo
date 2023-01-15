6ca344960177b66d5222c6e799aeb4201946b337
Yinon Polak <inon51@gmail.com>
2023-01-15 10:39:33 +0200

    reduce search space, set batch size 256 n_steps 8 n_epochs 10

--- rl_zoo3/hyperparams_opt.py
+++ rl_zoo3/hyperparams_opt.py
@@ -15,18 +15,18 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     :param trial:
     :return:
     """
-    batch_size = trial.suggest_categorical("batch_size", [128, 256])  # [8, 16, 32, 64, 128, 256, 512]
-    n_steps = trial.suggest_categorical("n_steps", [8, 16])  # [8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
-    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 0.1)
+    # batch_size = trial.suggest_categorical("batch_size", [256])  # [8, 16, 32, 64, 128, 256, 512]
+    # n_steps = trial.suggest_categorical("n_steps", [8, 16])  # [8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+    gamma = trial.suggest_categorical("gamma", [0.95, 0.98, 0.99])
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-4)
     lr_schedule = "constant"
     # Uncomment to enable learning rate schedule
     # lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
     ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
     clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
-    n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
-    gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
-    max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
+    # n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
+    gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95])
+    max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.9, 1, 2, 5])
     vf_coef = trial.suggest_uniform("vf_coef", 0, 1)
     # net_arch = trial.suggest_categorical("net_arch", ["small", "medium"])
     # Uncomment for gSDE (continuous actions)
@@ -39,9 +39,9 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     # activation_fn = trial.suggest_categorical('activation_fn', ['tanh', 'relu', 'elu', 'leaky_relu'])
     activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
 
-    # TODO: account when using multiple envs
-    if batch_size > n_steps:
-        batch_size = n_steps
+    # # TODO: account when using multiple envs
+    # if batch_size > n_steps:
+    #     batch_size = n_steps
 
     if lr_schedule == "linear":
         learning_rate = linear_schedule(learning_rate)
@@ -57,13 +57,13 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU, "elu": nn.ELU, "leaky_relu": nn.LeakyReLU}[activation_fn]
 
     return {
-        "n_steps": n_steps,
-        "batch_size": batch_size,
+        # "n_steps": n_steps,
+        # "batch_size": batch_size,
         "gamma": gamma,
         "learning_rate": learning_rate,
         "ent_coef": ent_coef,
         "clip_range": clip_range,
-        "n_epochs": n_epochs,
+        # "n_epochs": n_epochs,
         "gae_lambda": gae_lambda,
         "max_grad_norm": max_grad_norm,
         "vf_coef": vf_coef,
